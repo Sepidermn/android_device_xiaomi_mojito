19dbc2b78bb6c608f61332eff98bef12fe5b9be6
Sepidermn <zaidanagusta319@gmail.com>
2025-02-16 19:17:16 +0700

    mojito: bring up @OrionOS-Project

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/bliss_mojito.mk
+    $(LOCAL_DIR)/orion_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    bliss_mojito-user \
-    bliss_mojito-userdebug \
-    bliss_mojito-eng
+    orion_mojito-user \
+    orion_mojito-userdebug \
+    orion_mojito-eng
